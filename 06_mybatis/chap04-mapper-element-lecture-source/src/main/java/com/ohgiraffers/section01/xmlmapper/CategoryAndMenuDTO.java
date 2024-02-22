162ca90fb753fe4ca27ad7ab637aba5522d81825
LAPTOP-SB0CM2VU\Playdata <wjdxodndjaak@naver.com>
2024-02-22 14:05:26 +0900

    mapperelement

---
+++ 06_mybatis/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/CategoryAndMenuDTO.java
@@ -0,0 +1,55 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+import java.util.List;
+
+public class CategoryAndMenuDTO {
+    private int categoryCode;
+    private String categoryName;
+    private Integer refCategoryCode;
+    private List<MenuDTO> menus;
+
+    public CategoryAndMenuDTO() {
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getRefCategoryCode() {
+        return refCategoryCode;
+    }
+
+    public void setRefCategoryCode(Integer refCategoryCode) {
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    public List<MenuDTO> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<MenuDTO> menus) {
+        this.menus = menus;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryAndMenuDTO{" +
+                "categoryCode=" + categoryCode +
+                ", categoryName='" + categoryName + '\'' +
+                ", refCategoryCode=" + refCategoryCode +
+                ", menus=" + menus +
+                '}';
+    }
+}
